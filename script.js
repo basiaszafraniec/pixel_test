02f6a8fee697aaed6fffb73379d9c719d851b208
basiaszafraniec <basia.szafraniec@gmail.com>
2022-11-21 14:16:12 +0100

    works fine

--- script.js
+++ script.js
@@ -7,14 +7,22 @@ window.addEventListener('load', () => {
     class Particle {
         constructor(effect, x, y, color) {
             this.effect = effect;
-            this.x = Math.floor(x);
-            this.y = Math.floor(y);
+            this.x = Math.random() * this.effect.width;
+            this.y = Math.random() * this.effect.height;
             this.originX = Math.floor(x);
             this.originY = Math.floor(y);
-            this.color  = color;
-            this.size = effect.gap;
             this.speedX = 0;
             this.speedY = 0;
+            this.friction = 0.9;
+            this.color = color;
+            this.size = effect.gap + 1;
+            this.ease = 0.1;
+            this.distX = 0;
+            this.distY = 0;
+            this.distance = 0;
+            this.force = 0;
+            this.angle = 0;
+
         }
 
         draw(context) {
@@ -22,8 +30,19 @@ window.addEventListener('load', () => {
             context.fillRect(this.x, this.y, this.size, this.size)
         }
         update() {
-            this.x += this.originX - this.x;
-            this.y += this.originY - this.y;
+            this.distX = this.effect.mouse.x - this.x;
+            this.distY = this.effect.mouse.y - this.y;
+            this.distance = Math.sqrt(this.distX ** 2 + this.distY ** 2);
+            this.force = -this.effect.mouse.radius / this.distance;
+
+            if (this.distance < this.effect.mouse.radius) {
+                this.angle = Math.atan2(this.distY,this.distX);
+                this.speedX += this.force * Math.cos(this.angle);
+                this.speedY += this.force * Math.sin(this.angle);
+            }
+
+            this.x += (this.speedX *= this.friction) + (this.originX - this.x) * this.ease;
+            this.y += (this.speedY *= this.friction) + (this.originY - this.y) * this.ease;
         }
     }
 
@@ -35,10 +54,19 @@ window.addEventListener('load', () => {
             this.image = document.getElementById('angler');
             this.imageX = this.width / 2 - this.image.width / 2;
             this.imageY = this.height / 2 - this.image.height / 2;
-            this.gap = 3 ;
+            this.gap = 3;
+            this.mouse = {
+                radius: 100,
+                x: undefined,
+                y: undefined
+            }
+            window.addEventListener('mousemove', (e) => {
+                this.mouse.x = e.x;
+                this.mouse.y = e.y;
+            })
         }
         init(context) {
-            context.drawImage(this.image,this.imageX,this.imageY)
+            context.drawImage(this.image, this.imageX, this.imageY)
             const pixels = context.getImageData(0, 0, canvas_width, canvas_height).data;
             for (let y = 0; y < this.height; y += this.gap) {
                 for (let x = 0; x < this.width; x += this.gap) {
